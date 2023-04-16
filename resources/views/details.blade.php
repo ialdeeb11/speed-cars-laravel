295b3536f262b0c1bb6f083b58260db882218b33
ialdeeb11 <11.ialdeeb@gmail.com>
2023-04-16 19:41:54 +0300

    commit 13

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -4,52 +4,55 @@
 
 <div class="w3-content" style="max-width:1532px;">
 
-<h1 style="margin-left: 75px ;margin-top: 100px;" class="w3-sans-serif rounded mx-auto d-block" >Hyndai Accent 1995 - 3000$</h1>
+<h1 style="margin-left: 75px ;margin-top: 100px;" class="w3-sans-serif rounded mx-auto d-block" >{{$carMake}} {{$model}} {{$year}} - {{$price}}$</h1>
 
-<img src="photos/accent 1995.webp" alt="accent 1995" calss="rounded float-start" style="margin-left: 75px; float: left;">
+<img src="{{$path}}" alt="{{$model}} {{$year}}" class="rounded float-start main-image" >
+
+
+
 
 
 <table class="table table-sm" style="margin-top: 75px; float:left;">
  
     <tr>
       <td>Car Make</td>
-      <td>Hyundai</td>
+      <td>{{$carMake}}</td>
     </tr>
     <tr>
         <td>Model</td>
-        <td>Accent</td>
+        <td>{{$model}}</td>
     </tr>
     <tr>
       <td>Year</td>
-      <td>1995</td>
+      <td>{{$year}}</td>
     </tr>
     <tr>
         <td>Transmission</td>
-        <td>Manual</td>
+        <td>{{$gear}}</td>
     </tr>
     <tr>
         <td>Color</td>
-        <td>White</td>
+        <td>{{$color}}</td>
     </tr>
     <tr>
         <td>Car License</td>
-        <td>Licensed</td>
+        <td>{{$license}}</td>
     </tr>
     <tr>
         <td>Fuel</td>
-        <td>Gasoline</td>
+        <td>{{$fuel}}</td>
     </tr>
     <tr>
         <td>Conndition</td>
-        <td>Used</td>
+        <td>{{$used}}</td>
     </tr>
     <tr>
         <td>Car Customs</td>
-        <td>With Customs</td>
+        <td>{{$customs}}</td>
     </tr>
     <tr>
         <td>Insurance</td>
-        <td>Compulsory Insurance</td>
+        <td>{{$insurance}}</td>
     </tr>
 
   </table>
